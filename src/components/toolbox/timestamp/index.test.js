23734b229444fe864f5d5a8e0c549f454134a6d3
reyraa <ali@lightcurve.io>
2021-01-15 13:39:50 +0100

    Try with new Date()

--- src/components/toolbox/timestamp/index.test.js
+++ src/components/toolbox/timestamp/index.test.js
@@ -11,15 +11,15 @@ describe('components/toolbox/timestamp', () => {
 
   describe('Time', () => {
     it('renders "5 months" if today is 2017-01-15', () => {
-      jest.spyOn(global.Date, 'now').mockImplementation(() => Date.UTC(2017, 1, 15));
+      jest.spyOn(global.Date, 'now').mockImplementation(() => new Date('2017-01-15T00:00:00Z'));
       const wrapper = mount(<Time label={inputValue} />);
-      expect(wrapper).toHaveText('5 months');
+      expect(wrapper).toHaveText('6 months');
     });
   });
 
   describe('DateFromTimestamp', () => {
     it('renders "Jul 14, 2017"', () => {
-      jest.spyOn(global.Date, 'now').mockImplementation(() => Date.UTC(2017, 1, 15));
+      jest.spyOn(global.Date, 'now').mockImplementation(() => new Date('2017-01-15T00:00:00Z'));
       const wrapper = mount(<DateFromTimestamp time={inputValue} />);
       expect(wrapper).toHaveText('Jul 14, 2017');
     });
@@ -27,7 +27,7 @@ describe('components/toolbox/timestamp', () => {
 
   describe('TimeFromTimestamp', () => {
     it('renders text matching 12:00:00 AM', () => {
-      jest.spyOn(global.Date, 'now').mockImplementation(() => Date.UTC(2017, 1, 15));
+      jest.spyOn(global.Date, 'now').mockImplementation(() => new Date('2017-01-15T00:00:00Z'));
       const wrapper = mount(<TimeFromTimestamp time={inputValue} />);
       expect(wrapper.text()).toMatch('12:00:00 AM');
     });
